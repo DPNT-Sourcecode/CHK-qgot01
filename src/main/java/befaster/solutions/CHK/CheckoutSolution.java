cea927cdf748c551d9eaf2381e0312007719f72b
sbx_user1051 <sbx_user1051@169.254.40.153>
2019-09-06 11:45:30 +0000

    Fri Sep 06 11:45:30 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,8 +5,25 @@ import befaster.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
     	System.out.println("  :::: "+skus);
-    	return 115;
+    	int total=0;
+    	char[] c=skus.toCharArray();
+    	for(int i=0;i<c.length;i++){
+    		if(c[i]=='A' || c[i]=='a'){
+    			total=total+50;
+    		}
+    		else if(c[i]=='B' || c[i]=='b'){
+    			total=total+30;
+    		}
+    		else if(c[i]=='C' || c[i]=='c'){
+    			total=total+20;
+    		}
+    		else if(c[i]=='D' || c[i]=='d'){
+    			total=total+15;
+    		}
+    	}
+    	return total;
         //throw new SolutionNotImplementedException();
     }
 }
+
 
