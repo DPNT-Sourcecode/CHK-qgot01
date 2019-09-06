4cc2aeb98615a880c36625ebbd7efda9036d09b1
sbx_user1051 <sbx_user1051@169.254.40.153>
2019-09-06 11:48:30 +0000

    Fri Sep 06 11:48:30 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,13 +6,16 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
     	System.out.println("  :::: "+skus);
     	int total=0;
+    	int ACount=0,BCount=0;
     	char[] c=skus.toCharArray();
     	for(int i=0;i<c.length;i++){
     		if(c[i]=='A' || c[i]=='a'){
-    			total=total+50;
+    			ACount=ACount+1;
+    			//total=total+50;
     		}
     		else if(c[i]=='B' || c[i]=='b'){
-    			total=total+30;
+    			BCount=BCount+1;
+    			//total=total+30;
     		}
     		else if(c[i]=='C' || c[i]=='c'){
     			total=total+20;
@@ -25,5 +28,6 @@ public class CheckoutSolution {
         //throw new SolutionNotImplementedException();
     }
 }
+
 
 
