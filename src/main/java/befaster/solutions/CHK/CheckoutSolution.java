9f610c27a27956be655ac0cfbe48965b1682b342
sbx_user1051 <sbx_user1051@169.254.40.153>
2019-09-06 11:55:58 +0000

    Fri Sep 06 11:55:58 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -9,33 +9,30 @@ public class CheckoutSolution {
     	int ACount=0,BCount=0;
     	char[] c=skus.toCharArray();
     	if(skus.length()==0){
-    		return -1;
+    		return 0;
     	}
     	for(int i=0;i<c.length;i++){
-    		if(c[i]=='A' || c[i]=='a'){
+    		if(c[i]=='A'){
     			ACount=ACount+1;
     			//total=total+50;
     		}
-    		else if(c[i]=='B' || c[i]=='b'){
+    		else if(c[i]=='B'){
     			BCount=BCount+1;
     			//total=total+30;
     		}
-    		else if(c[i]=='C' || c[i]=='c'){
+    		else if(c[i]=='C'){
     			total=total+20;
     		}
-    		else if(c[i]=='D' || c[i]=='d'){
+    		else if(c[i]=='D'){
     			total=total+15;
+    		}else{
+    			return -1;
     		}
     	}
     	total=total+((ACount/3)*130)+((ACount%3)*50);
-    	total=total+((BCount/2)*45)+((BCount%2)*50);
+    	total=total+((BCount/2)*45)+((BCount%2)*30);
     	
     	return total;
         //throw new SolutionNotImplementedException();
     }
 }
-
-
-
-
-
